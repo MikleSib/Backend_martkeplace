cfb17977ad8263e45d1bc6d2ffe7ed7b79314e02
MikleSib <Mishatrof21@gmail.com>
2025-05-01 21:57:10 +0700

    MeGA FIX

--- backend/services/auth_service/jwt/__init__.py
+++ backend/services/auth_service/jwt/__init__.py
@@ -1,3 +1,3 @@
-from .jwt import create_access_token, create_refresh_token, verify_access_token
+from .jwt import create_access_token, create_refresh_token, verify_access_token, verify_refresh_token
 
-__all__ = ["create_access_token", "create_refresh_token", "verify_access_token"]
+__all__ = ["create_access_token", "create_refresh_token", "verify_access_token", "verify_refresh_token"]
